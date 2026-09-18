cbad9cbc809fd4787c7531b016b977e9de8f3038
Fredorostr <federiko@MacBookAir.res.rea.ru>
2026-09-18 12:49:32 +0300

    I hope it work

---
+++ pract_2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+int main() {
+    double first_num;
+    double sec_num;
+    char operation;
+    double result;
+    std::string exit;
+    long long factorial;
+    std::string ask;
+    std::cout<<"Вас приветсвует калькулятор !\n ";
+
+    while(true){
+        while(true){ 
+            std::cout<<"Введите операцию из следующего списка \n '*' = умножение \n '/' = деление \n '+' = сложение \n '-' = вычитание \n '%' = 1 процент \n '^' = возвести в квадрат \n '@' = найти корень \n ':' = возвести в степень любую \n '!' = найти факториал \n если не хотите что либо делать то введите no \n";
+            std::cin>>operation;
+            std::cout<<"если хотите выйти то введите exit(если нет то просто введите no) \n";
+            std::cin>>exit;
+            if (operation == '*' || operation == '/' || operation == '+' || operation == '-' || operation == '%' || operation == '^' || operation == '@' || operation == ':' || operation == '!') {
+                break;
+            }
+            if ( exit == "exit") {
+                return 0;
+            }
+        }
+        switch (operation) {
+            case '*':
+                std::cout<<"Введите первое число \n";
+                std::cin>>first_num;
+                std::cout<<"Введите второе число \n"; 
+                std::cin>>sec_num;
+                std::cout<<sec_num*first_num;
+                break;
+            case'/':
+                std::cout<<"введите делимое \n";
+                std::cin>>first_num;
+                std::cout<<"введите делитель \n"; 
+                std::cin>>sec_num;
+                std::cout<<first_num/sec_num;
+                break;
+            case'+':
+                std::cout<<"Введите первое число \n";
+                std::cin>>first_num;
+                std::cout<<"Введите второе число \n"; 
+                std::cin>>sec_num;
+                std::cout<<first_num+sec_num;
+                break;
+            case '-':
+                std::cout<<"Введите первое число \n";
+                std::cin>>first_num;
+                std::cout<<"Введите второе число \n"; 
+                std::cin>>sec_num;
+                break;
+            case'%':
+                std::cout<<"Число процент которого нужно найти \n";
+                std::cin>>first_num;
+                std::cout<<first_num/100;
+                break;
+            case'^':
+                std::cout<<"Введите число для возведения \n";
+                std::cin>>first_num;
+                std::cout<<first_num*first_num;
+                break;
+            case'@':
+                std::cout<<"Введите число для корня \n";
+                std::cin>>first_num;
+                result = sqrt(first_num);
+                std::cout<<result;
+                break;
+            case':':
+                std::cout<<"Введите первое число которое надо возвести в степень\n";
+                std::cin>>first_num;
+                std::cout<<"Степень в которую надо возвести \n"; 
+                std::cin>>sec_num;
+                result = 1;
+                for (int i = 0; i < sec_num; i++){
+                    result *= first_num;
+                }
+                break;
+            case'!':
+                std::cout<<"Введите число для нахождения факториала \n";
+                std::cin>>first_num;
+                factorial = 1;
+                for ( int i = 1; i <= first_num; i++){
+                    factorial *= i;
+                }
+                std::cout<<factorial;
+            break;
+        }  
+    std::cout<<"Хотите сделать что то ещё ? введите да или нет";
+    std::cin>>exit;
+    if( exit == "нет"){
+            return 0;
+    }
+    }
+}
